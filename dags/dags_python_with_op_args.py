e27a9532f1fcbb600316177a9d4a4508106a2ab4
Chulwoo Jung <sdfghfwwz@gmail.com>
2025-06-09 19:08:14 +0200

    Modified python_with_op_args and .env

--- dags/dags_python_with_op_args.py
+++ dags/dags_python_with_op_args.py
@@ -1,6 +1,7 @@
 from airflow.sdk import DAG
 from airflow.providers.standard.operators.python import PythonOperator
 import pendulum
+from common.common_func import regist
 
 with DAG(
     dag_id = 'dags_python_with_op_args',
@@ -8,10 +9,6 @@ with DAG(
     start_date = pendulum.datetime(2025, 1, 1, tz="Europe/Berlin"),
     catchup = False
 ) as dag:
-    
-    def regist(name, sex, *args):
-        print(f'name: {name}, sex: {sex}')
-        print(f'args: {args}')
     
     python_t1 = PythonOperator(
         task_id = 'python_t1',
--- plugins/common/common_func.py
+++ plugins/common/common_func.py
@@ -1,2 +1,7 @@
 def get_stfp():
     print("start stfp")
+
+
+def regist(name, sex, *args):
+    print(f'name: {name}, sex: {sex}')
+    print(f'args: {args}')
