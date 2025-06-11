7747863e70d7b3062a697d45db242100e35dcbcf
Chulwoo Jung <sdfghfwwz@gmail.com>
2025-06-11 22:52:49 +0200

    python email xcom UPLOAD

---
+++ dags/dags_python_email_xcom.py
@@ -0,0 +1,25 @@
+from airflow.sdk import DAG, task
+from airflow.providers.standard.operators.python import PythonOperator
+from airflow.providers.smtp.operators.smtp import EmailOperator
+
+with DAG(
+    dag_id='dags_python_email_xcom',
+    schedule='30 6 * * *',
+    start_date=pendulum.datetime(2025, 6, 1, tz='Europe/Berlin'),
+) as dag:
+    
+    @task(task_id='some_logic')
+    def some_logic(**kwargs):
+        from random import choice
+        return choice(['그렇습니다', '아닙니다'])
+    
+
+    send_email = EmailOperator(
+        task_id = 'send_email',
+        to = 'diazepam57@gmail.com',
+        subject = '{{ data_interval_end.in_timezone("Europe/Berlin") | ds }} 수민이는 바보인가요?',
+        html_content = '{{ data_interval_end.in_timezone("Europe/Berlin") | ds }} , 오늘은 <br> \
+                        {{ ti.xcom_pull(key="return_value", task_ids="some_logic")}} <b>하하</b>'
+    )
+
+    some_logic() >> send_email
